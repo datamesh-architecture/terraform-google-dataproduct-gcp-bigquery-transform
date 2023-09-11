54ad51ea383199851dfb05a72d7176906c2dd88e
Stefan Negele <stefan.negele@innoq.com>
2023-09-11 12:58:07 +0200

    Remove deprecated template provider

--- main.tf
+++ main.tf
@@ -4,21 +4,8 @@ provider "google" {
 }
 
 locals {
-  sql_in_directory    = "${path.root}/sql"
-  sqlTemplateInput    = fileset(local.sql_in_directory, "*.sql.tftpl")
-}
-
-data "local_file" "input_template" {
-  for_each = local.sqlTemplateInput
-  filename = "${local.sql_in_directory}/${each.key}"
-}
-
-data "template_file" "transform_sql" {
-  for_each = data.local_file.input_template
-  template = each.value.content
-  vars = {
-      source_table = var.input.source_table
-  }
+  sql_in_directory  = "${path.root}/sql"
+  sqlTemplateInput  = fileset(local.sql_in_directory, "*.sql.tftpl")
 }
 
 resource "google_bigquery_dataset" "dataset" {
@@ -28,12 +15,14 @@ resource "google_bigquery_dataset" "dataset" {
 }
 
 resource "google_bigquery_table" "view-dataproduct" {
-  for_each = data.template_file.transform_sql
+  for_each = local.sqlTemplateInput
 
   dataset_id = google_bigquery_dataset.dataset.dataset_id
   table_id   = "view-dataproduct-${split(".", each.key)[0]}"
-  view       {
-        query = each.value.rendered
-        use_legacy_sql = false
+  view {
+    query = templatefile("${local.sql_in_directory}/${each.key}", {
+      source_table = var.input.source_table
+    })
+    use_legacy_sql = false
   }
 }
--- providers.tf
+++ providers.tf
@@ -9,10 +9,6 @@ terraform {
       source = "hashicorp/archive"
       version = ">=2.4.0"
     }
-    template = {
-      source = "hashicorp/template"
-      version = ">=2.2.0"
-    }
     local = {
       source = "hashicorp/local"
       version = ">=2.4.0"
